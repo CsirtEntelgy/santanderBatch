7b6096d7ec1ea757110f3bb208623280c20784b4
samuel <samuel.9401@gmail.com>
2017-10-10 12:29:01 -0500

    Clases batch complemento pago

---
+++ src/com/interfactura/firmalocal/main/MassiveReadComplementoPago.java
@@ -0,0 +1,37 @@
+package com.interfactura.firmalocal.main;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.interfactura.firmalocal.controllers.MassiveReadController;
+
+public class MassiveReadComplementoPago {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try 
+		{
+			
+			AbstractApplicationContext context = new ClassPathXmlApplicationContext("app-config.xml", MassiveReadComplementoPago.class);
+			BeanFactory factory = (BeanFactory) context;
+			context.registerShutdownHook();
+			
+			MassiveReadController massiveRead = (MassiveReadController) factory.getBean(MassiveReadController.class);
+			massiveRead.createIdFileProcess();		
+			context.close();
+			
+			System.out.println("Fin del procesamiento Identifica Peticiones Massive Report");
+			System.exit(0);
+		} 
+		catch (Throwable e) 
+		{
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+}
