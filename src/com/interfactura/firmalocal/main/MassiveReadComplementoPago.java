ffa8e7a256f7caac8bc2f8873e80ec241ae656a1
samuel <samuel.9401@gmail.com>
2017-10-10 12:59:15 -0500

    Clases batch complemento pago

--- src/com/interfactura/firmalocal/main/MassiveReadComplementoPago.java
+++ src/com/interfactura/firmalocal/main/MassiveReadComplementoPago.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.interfactura.firmalocal.controllers.MassiveReadController;
+import com.interfactura.firmalocal.controllers.MassiveReadComplementoPagoController;
 
 public class MassiveReadComplementoPago {
 
@@ -13,22 +13,21 @@ public class MassiveReadComplementoPago {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try 
-		{
-			
-			AbstractApplicationContext context = new ClassPathXmlApplicationContext("app-config.xml", MassiveReadComplementoPago.class);
+		try {
+
+			AbstractApplicationContext context = new ClassPathXmlApplicationContext("app-config.xml",
+					MassiveReadComplementoPago.class);
 			BeanFactory factory = (BeanFactory) context;
 			context.registerShutdownHook();
-			
-			MassiveReadController massiveRead = (MassiveReadController) factory.getBean(MassiveReadController.class);
-			massiveRead.createIdFileProcess();		
+
+			MassiveReadComplementoPagoController massiveRead = (MassiveReadComplementoPagoController) factory
+					.getBean(MassiveReadComplementoPagoController.class);
+			massiveRead.createIdFileProcess();
 			context.close();
-			
+
 			System.out.println("Fin del procesamiento Identifica Peticiones Massive Report");
 			System.exit(0);
-		} 
-		catch (Throwable e) 
-		{
+		} catch (Throwable e) {
 			e.printStackTrace();
 			System.exit(-1);
 		}
